5f01043c6fe2f14caef5ead7163f850f38cd01bc
Dominic <Domnick080@outlook.com>
2021-10-01 10:39:14 +0100

    Add files via upload

---
+++ webDesignOne/src/app.js
@@ -0,0 +1,27 @@
+const screen1 = document.getElementById('screen1')
+const screen2 = document.getElementById('screen2')
+const ind1 = document.getElementById('ind1')
+const ind2 = document.getElementById('ind2')
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+
+let check = 1
+
+const change = () => {
+    if (check == 1) {
+        screen1.src = './images/slider2.jpg'
+        ind1.classList.toggle('active')
+        ind2.classList.toggle('active')
+        check = 0
+    } else {
+        screen1.src = './images/slider1.jpg'
+        ind1.classList.toggle('active')
+        ind2.classList.toggle('active')
+        check = 1
+    }
+}
+
+btn1.addEventListener('click', change)
+btn2.addEventListener('click', change)
+
+setInterval(change, 5000);
